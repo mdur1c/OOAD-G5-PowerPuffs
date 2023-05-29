51d3f7d659060bb074ca720b62313d6f7561b102
apehlivan1 <apehlivan1@etf.unsa.ba>
2023-05-29 21:37:20 +0200

    Scaffolding & HomeView

---
+++ EduConnect/Controllers/StatisticsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EduConnect.Data;
+using EduConnect.Models;
+
+namespace EduConnect.Controllers
+{
+    public class StatisticsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StatisticsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Statistics
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Statistics.ToListAsync());
+        }
+
+        // GET: Statistics/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var statistics = await _context.Statistics
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (statistics == null)
+            {
+                return NotFound();
+            }
+
+            return View(statistics);
+        }
+
+        // GET: Statistics/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Statistics/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id")] Statistics statistics)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(statistics);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(statistics);
+        }
+
+        // GET: Statistics/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var statistics = await _context.Statistics.FindAsync(id);
+            if (statistics == null)
+            {
+                return NotFound();
+            }
+            return View(statistics);
+        }
+
+        // POST: Statistics/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id")] Statistics statistics)
+        {
+            if (id != statistics.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(statistics);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!StatisticsExists(statistics.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(statistics);
+        }
+
+        // GET: Statistics/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var statistics = await _context.Statistics
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (statistics == null)
+            {
+                return NotFound();
+            }
+
+            return View(statistics);
+        }
+
+        // POST: Statistics/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var statistics = await _context.Statistics.FindAsync(id);
+            _context.Statistics.Remove(statistics);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool StatisticsExists(int id)
+        {
+            return _context.Statistics.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ EduConnect/Controllers/TutorsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using EduConnect.Data;
+using EduConnect.Models;
+
+namespace EduConnect.Controllers
+{
+    public class TutorsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TutorsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Tutors
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Tutors.ToListAsync());
+        }
+
+        // GET: Tutors/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tutor = await _context.Tutors
+                .FirstOrDefaultAsync(m => m.Username == id);
+            if (tutor == null)
+            {
+                return NotFound();
+            }
+
+            return View(tutor);
+        }
+
+        // GET: Tutors/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Tutors/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("StatisticsId,Username,Password,Name,Surname,Email,PhoneNumber,City")] Tutor tutor)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(tutor);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tutor);
+        }
+
+        // GET: Tutors/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tutor = await _context.Tutors.FindAsync(id);
+            if (tutor == null)
+            {
+                return NotFound();
+            }
+            return View(tutor);
+        }
+
+        // POST: Tutors/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("StatisticsId,Username,Password,Name,Surname,Email,PhoneNumber,City")] Tutor tutor)
+        {
+            if (id != tutor.Username)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(tutor);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TutorExists(tutor.Username))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(tutor);
+        }
+
+        // GET: Tutors/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tutor = await _context.Tutors
+                .FirstOrDefaultAsync(m => m.Username == id);
+            if (tutor == null)
+            {
+                return NotFound();
+            }
+
+            return View(tutor);
+        }
+
+        // POST: Tutors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var tutor = await _context.Tutors.FindAsync(id);
+            _context.Tutors.Remove(tutor);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TutorExists(string id)
+        {
+            return _context.Tutors.Any(e => e.Username == id);
+        }
+    }
+}
